41ff91d0a57d6e820c570645b57da6f9d00c674b
Andy <andy.su@outlook.com>
2018-04-16 01:48:07 -0400

    Store user data in user area.

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -9,7 +9,8 @@ export const addExpense = expense => ({
 
 export const startAddExpense = (expenseData = {}) => {
   // only works due to redux-thunk
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = "",
       note = "",
@@ -25,7 +26,7 @@ export const startAddExpense = (expenseData = {}) => {
     };
 
     return database
-      .ref("expenses")
+      .ref(`users/${uid}/expenses`)
       .push(expense)
       .then(ref => {
         dispatch(
@@ -45,9 +46,10 @@ export const removeExpense = id => ({
 });
 
 export const startRemoveExpense = id => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref(`expenses/${id}`)
+      .ref(`users/${uid}/expenses/${id}`)
       .remove()
       .then(() => {
         dispatch(removeExpense(id));
@@ -63,9 +65,10 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref(`expenses/${id}`)
+      .ref(`users/${uid}/expenses/${id}`)
       .update(updates)
       .then(() => {
         dispatch(editExpense(id, updates));
@@ -81,9 +84,10 @@ export const setExpenses = expenses => ({
 
 // export const startSetExpenses;
 export const startSetExpenses = () => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref("expenses")
+      .ref(`users/${uid}/expenses`)
       .once("value")
       .then(snapshot => {
         const expenses = [];
